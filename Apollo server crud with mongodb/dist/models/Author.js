884235c866fa4748c1edc073d3c3c12dfe5da593
rahul01-git <rahulneupane0090@gmail.com>
2023-08-18 11:34:57 +0545

    updating

--- Apollo server crud with mongodb/dist/models/Author.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export const AuthorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: Number,
-});
-export default mongoose.model('Author', AuthorSchema);
--- Apollo server crud with mongodb/src/graphql/typeDefs.ts
+++ Apollo server crud with mongodb/src/graphql/typeDefs.ts
@@ -12,12 +12,31 @@ export const typeDefs = `#graphql
         authorId: ID!
     }
 
+    type User{
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        createdAt: String!
+    }
+
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
+
     type Query{
         authors: [Author!]!
         books: [Book!]!
     }
 
     type Mutation{
+
+        register(registerInput: RegisterInput) : User!
+        login
+
         createAuthor(name: String!, age: Int) : Author!
         updateAuthor(id: ID!, name: String!, age:Int) : Author!
         deleteAuthor(id:ID!) : Author!
